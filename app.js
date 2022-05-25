5da4f7d112ae5f2128e958b7a786cc1c52f11de4
Jhonnatan Urquijo <j.urquijog1992@hotmail.com>
2022-05-25 18:53:59 -0500

    Arquitectura MVC e inicio de EJS

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const publicPath = path.resolve(__dirname, './public');
 
 app.use(express.static(publicPath));
 
+app.set('view engine', 'ejs');
+
 // Servidor escuchando
 app.listen(5000, () => {
     console.log('Servidor corriendo en el puerto 5000: http://localhost:5000/');
@@ -14,31 +16,31 @@ app.listen(5000, () => {
 
 // Rutas y vistas
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/home.html'));
+    res.sendFile(path.resolve(__dirname, './src/views/home.ejs'));
 })
 
 app.get('/shopping-cart', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/shopping-cart.html'));
+    res.sendFile(path.resolve(__dirname, './src/views/shopping-cart.ejs'));
 })
 
 app.get('/productDetail', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/productDetail.html'));
+    res.sendFile(path.resolve(__dirname, './src/views/productDetail.ejs'));
 })
 
 app.get('/register', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/register.html'));
+    res.sendFile(path.resolve(__dirname, './src/views/register.ejs'));
 })
 
 app.get('/login', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/login.html'));
+    res.sendFile(path.resolve(__dirname, './src/views/login.ejs'));
 })
 
 /* ---------------RUTAS DE LOS BOTONES EN FORMULARIOS LOGIN Y REGISTER--------------- */
 
 app.post("/register", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/home.html"));
+    res.sendFile(path.resolve(__dirname, "./src/views/home.ejs"));
 });
 
 app.post("/login", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/productDetail.html"));
+    res.sendFile(path.resolve(__dirname, "./src/views/productDetail.ejs"));
 });

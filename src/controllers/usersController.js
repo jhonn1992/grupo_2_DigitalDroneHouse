1c97b209f0668461e1f25ed730027cbba72f62fb
Brianbte <brianbte993@gmail.com>
2022-06-20 17:39:05 -0500

    El formulario de register queda almacenando la info de usuario incluyendo el multer para img perfil

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -16,6 +16,24 @@ const usersController = {
         let user = users.find((user) => user.id == id);
         res.render('user' , {user});
     },
+    userRegister: (req, res) => {
+        const users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
+
+        let newUser = {
+            id: users[users.length - 1].id + 1,
+
+            name: req.body.name,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            password: req.body.password,
+            rol: req.body.rol,
+            avatar: req.file ? req.file.filename : "defaultAvatar.png"
+
+          };
+          users.push(newUser);
+          fs.writeFileSync(usersFilePath, JSON.stringify(users, null, " "));
+          res.redirect("/");
+        },
     login: (req, res) => {
         res.render('login');
     }
--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path')
+const multer = require('multer');
+const storage = multer.diskStorage ({
+    destination: (req, file, cb) => {
+        cb(null, 'public/img/users');
+    },
+    filename: (req, file, cb) => {
+        let filename = `${Date.now()}_avatar${path.extname(file.originalname)}`;
+        cb(null, filename);
+    }
+})
+
+const uploadFile = multer({storage: storage});
 
 const usersController = require('../controllers/usersController');
 
 router.get('/register', usersController.register);
+router.post('/', uploadFile.single('avatar'),usersController.userRegister);
 router.get('/user/:id?', usersController.user);
-router.post('/user/id?', usersController.user);
 router.get('/login', usersController.login);
 
 module.exports = router;

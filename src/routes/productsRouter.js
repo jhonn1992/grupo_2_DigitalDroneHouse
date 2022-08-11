69b6e154e5b9b54fc86df9a7b03407d4223b212b
Jhonnatan Urquijo <j.urquijog1992@hotmail.com>
2022-08-11 09:20:49 -0500

    Se ajusta formulario de modificación de productos

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -27,11 +27,32 @@ const validations = [
     })
 ];
 
+const validationsProductEdit = [
+    check('nombre').notEmpty().withMessage('Tienes que escribir un nombre')
+                    .isLength({min: 5}).withMessage('El nombre debe tener minimo 5 caracteres'),
+    check('precio').notEmpty().withMessage('Tienes que escribir el precio'),
+    check('reference').notEmpty().withMessage('Tienes que escribir una referencia'),
+    check('categoria').notEmpty().withMessage('Tienes que seleccionar una categoría'),
+    check('imagen').custom((value, { req }) => {
+        let file = req.file;
+        let acceptedExtensions = ['.jpg', '.png', '.jpeg', '.gif'];
+        if(!file) {
+            
+        } else {
+            let fileExtesion =  path.extname(file.originalname);
+            if (!acceptedExtensions.includes(fileExtesion)) {
+                throw new Error(`Allowed extensions ${acceptedExtensions.join(', ')}`);
+            }
+        }
+        return true
+    })
+];
+
 router.get('/shopping-cart', productsController.shoppingCart);
 router.get('/shopping-cart/:id', productsController.shoppingCartProductDetail);
 router.get('/productDetail/:id', productsController.productDetail);
 router.get('/productEdit/:id', productsController.productEdit);
-router.put('/productEdit/:id', uploadFile.single('imagen'), validations, productsController.productUpdate);
+router.put('/productEdit/:id', uploadFile.single('imagen'), validationsProductEdit, productsController.productUpdate);
 router.get('/productCreate', productsController.productCreate);
 router.post('/', uploadFile.single('imagen'), validations, productsController.productCreatePOST);
 router.get('/', productsController.productList);

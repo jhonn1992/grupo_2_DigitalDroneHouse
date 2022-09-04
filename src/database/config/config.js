5ee693c20591a165808d80522a726ccee221b87f
JorgePolania <jorge.ed.polania@gmail.com>
2022-09-04 10:54:30 -0500

    modificaciones del Home

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "Mister2021*mysql",
+    "password": "1234",
     "database": "dronedigitalhousedb",
     "host": "127.0.0.1",
     "dialect": "mysql"
